5416eaff44ce8d811bbce2222545f3f73842c8d0
Dark2413 <116039916+Dark2413@users.noreply.github.com>
2022-10-18 17:43:22 -0400

    primeiro commit
    sites

---
+++ 2204/main/main.js
@@ -0,0 +1,92 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const h1 = document.querySelector('h1');
+
+//     h1.anclick = print
+
+//     function print() {
+//         console.log('print')
+
+
+//     }
+
+
+//     h1.anclick = function( ) {
+//         console.log('outro momento')
+//     }
+
+
+// const elements = document.getElementsByTagName('h1')
+// console.log(element)
+// elements.array.forEach(el => console.log (el))
+    
+
+
+
+
+// const element = document.querySelector('h1');
+
+// element.textContent += "Ola Devs!"
+
+// console.log();
+
+
+
+
+
+
+// const element = document.querySelector('input');
+
+// element.value ="uma coisa "
+
+
+// const header = dcument.querySelector('header')
+// header.setAtribute('id','header')
+
+// const headerID = document.querySelector('#header')
+
+// console.log(HeaderID.getAtribute('class'))
+
+
+
+// const element = document.querySelector('body')
+
+// element.style.backgroundcolor = 'blue'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
